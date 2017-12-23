a58025a921388f014b3fa7a7a81aae72956240b0
Madhav Bahl <madhavbahl10@gmail.com>
2017-12-23 14:35:05 +0530

    Add linear search

--- Search/README.md
+++ Search/README.md
@@ -2,7 +2,14 @@
 In computer science, a search algorithm is any algorithm which solves the Search problem, namely, to retrieve information stored within some data structure, or calculated in the search space of a problem domain.
 
 ### Some commonly used searching algorithms
-  1. [Linear Search]()
+  1. [Linear Search](linear.c)<br />
+<b>`Algorithm`</b>
+```
+STEP 1: Initialize the position variable as -1
+STEP 2: Start from the leftmost element of array and one by one compare x with each element of array
+STEP 3: If x matches with an element, store the value of index in position variable
+STEP 3: If the final value of position variable is -1 => the element does not exist
+```
   2. [Binary Search]()
   3. [Jump Search]()
   4. [Interpolation Search]()
---
+++ Search/linear.c
@@ -0,0 +1,30 @@
+/* =================================== */
+/* ===== Linear search algorithm ===== */
+/* =================================== */
+
+#include<stdio.h>
+
+int main() {
+    int arr[100],n,i,x,pos=-1;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d",&n);
+    printf("Enter the Elements: \n");
+    for(i=0;i<n;i++) {
+        printf("  arr[%d] = ",i);
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the number you want to search: ");
+    scanf("%d",&x);
+    // Search using a for loop
+    for(i=0;i<n;i++) {
+        if(x == arr[i]) {
+            pos=i;
+            break;
+        }
+    }
+    if(pos == -1) {
+        printf("\nThe given element is not present in the array!");
+    } else {
+        printf("\nThe index of given element is: %d", pos);
+    }
+}
