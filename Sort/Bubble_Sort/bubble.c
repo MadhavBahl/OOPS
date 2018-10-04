3414e9241997a089e54c085c00086829834f400a
Vaibhav Gupta <vaibhavgupta40@gmail.com>
2018-10-04 21:58:43 +0530

    Optimized Bubble sort.
    - Make implementation of array, dynamic.
    - In bubble sort algorithm, except worst case, the array gets sorted before the outer-loop has completed its iteration. Hence in that case, there is no need for outer-loop to complete its iteration.

--- Sort/Bubble_Sort/bubble.c
+++ Sort/Bubble_Sort/bubble.c
@@ -3,10 +3,13 @@
 /* ================================= */
 
 #include<stdio.h>
+#include<stdlib.h>
 int main() {
-    int n,arr[100],i,j,temp;
+    short swap;
+    int n,*arr,i,j,temp;
     printf("Enter the number of elements in the array: ");
     scanf("%d",&n);
+    arr = (int*) malloc(sizeof(int)*n);
     printf("Enter the Elements: \n");
     for(i=0;i<n;i++) {
         printf("  arr[%d] = ",i);
@@ -15,13 +18,20 @@ int main() {
     printf("Entered Array Is: ");
     for(i=0;i<n;i++)  printf("%d ",arr[i]);
     // Sort the array
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++) {
+        swap = 0;
         for(j=i+1;j<n;j++)
             if(arr[i]>arr[j]) {
+                swap = 1;
                 temp = arr[i];
                 arr[i]=arr[j];
                 arr[j]= temp;
             }
+        if(!swap) //it means the array has got sorted before outer-loop has completed.
+            break;    
+    }
     printf("\nSorted Array Is: ");
     for(i=0;i<n;i++)  printf("%d ",arr[i]);
+
+    free(arr);
 }
